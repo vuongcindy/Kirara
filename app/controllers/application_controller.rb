60e46d1adaf2c04220409b1db90b5322eaef5db6
Cindy Vuong <vuongcindytv@gmail.com>
2022-07-08 22:28:21 -0700

    Adds user model and updates routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :current_user, :logged_in?, :require_logged_in
+
+    private
+    def current_user
+        return nil if !session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        if !current_user
+            render json: { base: ['invalid credentials'] }, status: 404
+        end
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def logout!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    has_many :profiles,
+        primary_key: :id,
+        foreign_key: user_id:,
+        class_name: :Profile
+
+    def self.find_by_credentials(email, password)
+        @user = User.find_by(email: email)
+
+        if @user
+            if @user.is_password?(password)
+                return @user
+            end
+        end
+        nil
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+end
